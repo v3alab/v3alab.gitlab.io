9a0369c44e0721d87134496f4dcb49036a11f99b
unknown <v3alab@gmail.com>
2023-12-19 17:48:44 +1030

    Update

--- js/wowchemy-init.js
+++ js/wowchemy-init.js
@@ -1,5 +1,5 @@
 (() => {
-  // ns-hugo:C:\Users\PITATO\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20201211023654-f6090cd7e55d\assets\js\wowchemy-theming.js
+  // ns-hugo:C:\Users\willi\AppData\Local\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy@v0.0.0-20201211023654-f6090cd7e55d\assets\js\wowchemy-theming.js
   function getThemeMode() {
     return parseInt(localStorage.getItem("wcTheme") || 2);
   }
